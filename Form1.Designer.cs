c115f7fbb662ca3b88cc69f9eb1fc05ca16d1aee
Ximena Vazquez Mellado <ximevzquez@gmail.com>
2023-01-22 11:52:22 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace actividad3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(172, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(457, 291);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(253, 372);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(87, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(463, 343);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Translate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(240, 339);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Type the rotation angle:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(463, 372);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Rotate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(463, 401);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Rotate in origin";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace actividad3
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        Graphics g;
+        Point a, b, c, d;                   // Original square
+        Point north, south, east, west;     // Plane coordenates
+        Point new_a, new_b, new_c, new_d;   // Rotated square
+
+        double angle;
+        Point pivot, center; // Origin, center of square
+
+        private void button3_Click(object sender, EventArgs e) // Rotate square when pivot = center
+        {
+            angle = Convert.ToInt32(textBox1.Text);
+            angle = -(angle * Math.PI / 180);
+            rotate_origin();
+            g.Clear(Color.White);
+            cartesian_plane();
+            draw_square(new_a, new_b, new_c, new_d);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) // Around its own center
+        {
+            angle = Convert.ToInt32(textBox1.Text);
+            angle = -(angle * Math.PI / 180);
+            Rotation();
+            g.Clear(Color.White);
+            cartesian_plane();
+            draw_square(new_a, new_b, new_c, new_d);
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Around the origin
+        {
+            angle = Convert.ToInt32(textBox1.Text);
+            angle = -(angle * Math.PI / 180);
+            Translation();
+            g.Clear(Color.White);
+            cartesian_plane();
+            draw_square(new_a, new_b, new_c, new_d);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bmp);
+            pictureBox1.Image = bmp;
+
+            pivot = new Point(bmp.Width / 2, bmp.Height / 2);
+            center = new Point((bmp.Width / 2) + 50, (bmp.Height / 2) - 50);
+            cartesian_plane();
+
+            // Inicialize the square of size 100x100
+            a = new Point(pictureBox1.Width / 2, pictureBox1.Height / 2);
+            b = new Point((pictureBox1.Width / 2) + 100, pictureBox1.Height / 2);
+            c = new Point((pictureBox1.Width / 2) + 100, (pictureBox1.Height / 2) - 100);
+            d = new Point(pictureBox1.Width / 2, (pictureBox1.Height / 2) - 100);
+
+            draw_square(a, b, c, d);
+
+        }
+
+        private void draw_square(Point a, Point b, Point c, Point d)
+        {            
+
+            g.DrawLine(Pens.DarkViolet, a, b);
+            g.DrawLine(Pens.Green, b, c);
+            g.DrawLine(Pens.DarkViolet, c, d);
+            g.DrawLine(Pens.DarkViolet, d, a);
+
+            /*
+            Pen pen = new Pen(Color.DarkViolet, 1);
+            Rectangle rect = new Rectangle(pictureBox1.Width/2, pictureBox1.Height/2-100, 100, 100);
+            g.DrawRectangle(pen, rect);
+            */
+            pictureBox1.Invalidate();
+        }
+
+        private void cartesian_plane() // Draws the red cartesian plane in the background
+        {
+            north = new Point(pictureBox1.Width / 2, 0);
+            south = new Point(pictureBox1.Width / 2, pictureBox1.Height);
+            west = new Point(0, pictureBox1.Height / 2);
+            east = new Point(pictureBox1.Width, pictureBox1.Height / 2);
+            g.DrawLine(Pens.Red, north, south);
+            g.DrawLine(Pens.Red, east, west);
+        }
+
+        private void Translation() // Translates square around the center of the cartesian plane
+        {
+            new_a.X = (int)(((a.X - pivot.X) * Math.Cos(angle)) - ((a.Y - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            new_a.Y = (int)(((a.X - pivot.X) * Math.Sin(angle)) + ((a.Y - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+            new_b.X = (int)(((b.X - pivot.X) * Math.Cos(angle)) - ((b.Y - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            new_b.Y = (int)(((b.X - pivot.X) * Math.Sin(angle)) + ((b.Y - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+            new_c.X = (int)(((c.X - pivot.X) * Math.Cos(angle)) - ((c.Y - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            new_c.Y = (int)(((c.X - pivot.X) * Math.Sin(angle)) + ((c.Y - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+            new_d.X = (int)(((d.X - pivot.X) * Math.Cos(angle)) - ((d.Y - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            new_d.Y = (int)(((d.X - pivot.X) * Math.Sin(angle)) + ((d.Y - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+        }
+
+        private void Rotation () // Rotates square around its own center
+        {
+            new_a.X = (int)(((a.X - center.X) * Math.Cos(angle)) - ((a.Y - center.Y) * Math.Sin(angle)) + center.X);
+            new_a.Y = (int)(((a.X - center.X) * Math.Sin(angle)) + ((a.Y - center.Y) * Math.Cos(angle)) + center.Y);
+            new_b.X = (int)(((b.X - center.X) * Math.Cos(angle)) - ((b.Y - center.Y) * Math.Sin(angle)) + center.X);
+            new_b.Y = (int)(((b.X - center.X) * Math.Sin(angle)) + ((b.Y - center.Y) * Math.Cos(angle)) + center.Y);
+            new_c.X = (int)(((c.X - center.X) * Math.Cos(angle)) - ((c.Y - center.Y) * Math.Sin(angle)) + center.X);
+            new_c.Y = (int)(((c.X - center.X) * Math.Sin(angle)) + ((c.Y - center.Y) * Math.Cos(angle)) + center.Y);
+            new_d.X = (int)(((d.X - center.X) * Math.Cos(angle)) - ((d.Y - center.Y) * Math.Sin(angle)) + center.X);
+            new_d.Y = (int)(((d.X - center.X) * Math.Sin(angle)) + ((d.Y - center.Y) * Math.Cos(angle)) + center.Y);
+
+        }
+
+        private void rotate_origin() // places square's center in the pivot point and rotates
+        {
+            new_a.X = (int)(((a.X - 50 - pivot.X) * Math.Cos(angle)) - ((a.Y + 50 - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            new_a.Y = (int)(((a.X - 50 - pivot.X) * Math.Sin(angle)) + ((a.Y + 50 - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+            new_b.X = (int)(((b.X - 50 - pivot.X) * Math.Cos(angle)) - ((b.Y + 50 - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            new_b.Y = (int)(((b.X - 50 - pivot.X) * Math.Sin(angle)) + ((b.Y + 50 - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+            new_c.X = (int)(((c.X - 50 - pivot.X) * Math.Cos(angle)) - ((c.Y + 50 - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            new_c.Y = (int)(((c.X - 50 - pivot.X) * Math.Sin(angle)) + ((c.Y + 50 - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+            new_d.X = (int)(((d.X - 50 - pivot.X) * Math.Cos(angle)) - ((d.Y + 50 - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            new_d.Y = (int)(((d.X - 50 - pivot.X) * Math.Sin(angle)) + ((d.Y + 50 - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+        }
+
+    }
+}
